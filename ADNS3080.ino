e0918bfffd6da901cc5e57c35894430c94822d6c
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-09-07 17:46:36 +0200

    First commit

---
+++ ADNS3080.ino
@@ -0,0 +1,194 @@
+/* Copyright (C) 2015 Kristian Sloth Lauszus. All rights reserved.
+
+ Based on the code by Randy Mackay. DIYDrones.com
+
+ This code is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This code is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this code.  If not, see <http://www.gnu.org/licenses/>.
+
+ Contact information
+ -------------------
+
+ Kristian Sloth Lauszus
+ Web      :  http://www.lauszus.com
+ e-mail   :  lauszus@gmail.com
+*/
+
+#include <SPI.h>
+
+SPISettings spiSettings(2e6, MSBFIRST, SPI_MODE3); // 2 MHz, mode 3
+
+// Register Map for the ADNS3080 Optical OpticalFlow Sensor
+#define ADNS3080_PRODUCT_ID            0x00
+#define ADNS3080_MOTION                0x02
+#define ADNS3080_DELTA_X               0x03
+#define ADNS3080_DELTA_Y               0x04
+#define ADNS3080_SQUAL                 0x05
+#define ADNS3080_MOTION_CLEAR          0x12
+#define ADNS3080_FRAME_CAPTURE         0x13
+
+// ADNS3080 hardware config
+#define ADNS3080_PIXELS_X              30
+#define ADNS3080_PIXELS_Y              30
+
+// Id returned by ADNS3080_PRODUCT_ID register
+#define ADNS3080_PRODUCT_ID_VALUE      0x17
+
+static const uint8_t RESET_PIN = 9;
+static const uint8_t SS_PIN = SS; // Pin 10
+
+static int32_t x, y;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial); // Wait for serial port to open
+
+  SPI.begin();
+  pinMode(SS, OUTPUT);
+  digitalWrite(SS, HIGH);
+  pinMode(RESET_PIN, OUTPUT);
+  reset();
+  delay(10);
+
+  uint8_t id = spiRead(ADNS3080_PRODUCT_ID);
+  if (id == ADNS3080_PRODUCT_ID_VALUE)
+    Serial.println(F("ADNS-3080 found!"));
+  else {
+    Serial.print(F("Could not find ADNS3080: "));
+    Serial.println(id, HEX);
+    while (1);
+  }
+}
+
+void loop() {
+#if 1
+  updateSensor();
+#else
+  Serial.println(F("image data --------------"));
+  printPixelData();
+  Serial.println(F("-------------------------"));
+  delay(1000);
+#endif
+}
+
+// TODO: Implement configuration: https://github.com/diydrones/ardupilot/blob/5ddbcc296dd6dd9ac9ed6316ac3134c736ae8a78/libraries/AP_OpticalFlow/AP_OpticalFlow_ADNS3080.cpp#L68-L98
+// Also implement "update_conversion_factors"
+
+void printPixelData(void) {
+  bool isFirstPixel = true;
+
+  // Write to frame capture register to force capture of frame
+  spiWrite(ADNS3080_FRAME_CAPTURE, 0x83);
+
+  // Wait 3 frame periods + 10 nanoseconds for frame to be captured
+  delayMicroseconds(1510);  // min frame speed is 2000 frames/second so 1 frame = 500 nano seconds.  so 500 x 3 + 10 = 1510
+
+  // Display the pixel data
+  for (uint8_t i = 0; i < ADNS3080_PIXELS_Y; i++) {
+    for (uint8_t j = 0; j < ADNS3080_PIXELS_X; j++) {
+      uint8_t regValue = spiRead(ADNS3080_FRAME_CAPTURE);
+      if (isFirstPixel && !(regValue & 0x40))
+        Serial.println(F("Failed to find first pixel"));
+      isFirstPixel = false;
+      uint8_t pixelValue = regValue << 2;
+      Serial.print(pixelValue);
+      if (j != ADNS3080_PIXELS_X - 1)
+        Serial.write(',');
+      delayMicroseconds(50);
+    }
+    Serial.println();
+  }
+
+  // Hardware reset to restore sensor to normal operation
+  reset();
+}
+
+void updateSensor(void) {
+  // Read sensor
+  uint8_t surfaceQuality = spiRead(ADNS3080_SQUAL);
+  uint8_t motion = spiRead(ADNS3080_MOTION);
+  //Serial.print(motion & 0x01); // Resolution
+
+  if (motion & 0x10) // Check if we've had an overflow
+    Serial.println(F("OVERFLOW"));
+  else if (motion & 0x80) {
+    int8_t dx = spiRead(ADNS3080_DELTA_X);
+    int8_t dy = spiRead(ADNS3080_DELTA_Y);
+
+    x += dx;
+    y += dy;
+
+    // Print values
+    Serial.print(x);
+    Serial.write(',');
+    Serial.print(dx);
+    Serial.write('\t');
+    Serial.print(y);
+    Serial.write(',');
+    Serial.print(dy);
+    Serial.write('\t');
+    Serial.println(surfaceQuality);
+    Serial.flush();
+  }
+#if 0
+  else
+    Serial.println(motion, HEX);
+#endif
+
+  delay(10);
+}
+
+void reset(void) {
+  digitalWrite(RESET_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(RESET_PIN, LOW);
+}
+
+// Will cause the Delta_X, Delta_Y, and internal motion registers to be cleared
+void clearMotion() {
+  spiWrite(ADNS3080_MOTION_CLEAR, 0xFF); // Writing anything to this register will clear the sensor's motion registers
+  x = y = 0;
+}
+
+void spiWrite(uint8_t reg, uint8_t data) {
+  spiWrite(reg, &data, 1);
+}
+
+void spiWrite(uint8_t reg, uint8_t *data, uint8_t length) {
+  SPI.beginTransaction(spiSettings);
+  digitalWrite(SS_PIN, LOW);
+  SPI.transfer(reg | 0x80);
+  delayMicroseconds(50);
+  SPI.transfer(data, length);
+  digitalWrite(SS_PIN, HIGH);
+  SPI.endTransaction();
+  delayMicroseconds(50);
+}
+
+uint8_t spiRead(uint8_t reg) {
+  uint8_t buf;
+  spiRead(reg, &buf, 1);
+  return buf;
+}
+
+void spiRead(uint8_t reg, uint8_t *data, uint8_t length) {
+  SPI.beginTransaction(spiSettings);
+  digitalWrite(SS_PIN, LOW);
+  SPI.transfer(reg);
+  delayMicroseconds(50);
+  memset(data, 0, length);
+  SPI.transfer(data, length);
+  digitalWrite(SS_PIN, HIGH);
+  SPI.endTransaction();
+  delayMicroseconds(50);
+}
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# ADNS-3080 example code
+#### Developed by Kristian Sloth Lauszus, 2015
+
+The code is released under the GNU General Public License.
+_________
+
+This is an example code I wrote in order to test the ADNS-3080 optical flow sensor. It was inspired by this code provided by DIY Drones: <https://github.com/diydrones/ardupilot/tree/5ddbcc296dd6dd9ac9ed6316ac3134c736ae8a78/libraries/AP_OpticalFlow> written by Randy Mackay.
+
+The Python program [ADNS3080ImageGrabber.py](ADNS3080ImageGrabber.py) was copied from this [page](https://github.com/diydrones/ardupilot/tree/5ddbcc296dd6dd9ac9ed6316ac3134c736ae8a78/libraries/AP_OpticalFlow/examples/ADNS3080ImageGrabber).
+
+More information about this sensor can be found at the following page: <http://copter.ardupilot.com/wiki/common-mouse-based-optical-flow-sensor-adns3080> and in the [datasheet](adns_3080.pdf)
+
+For more information send me an email at <lauszus@gmail.com>.
